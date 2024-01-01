fe8aa8a65b76b3baa937b12c5109360a66a88bf4
masidda <128308214+masidda@users.noreply.github.com>
2024-01-01 23:41:07 +0900

    Add files via upload

---
+++ qude/js/main.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    //01. gnb animation
+
+    const menuOpen = document.querySelector('.gnb .menuOpen');
+    const menuBox = document.querySelector('.gnb .menuBox');
+
+    menuOpen.addEventListener('click', () => {
+        menuBox.classList.toggle('on');
+    })
+}
